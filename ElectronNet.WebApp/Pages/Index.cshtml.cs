64dec445f2d8e14eca2f47a500a46226cb3f61cb
TSIBSPROC\romulo.teixeira <romulo.teixeira@sibs.com>
2021-03-08 19:11:43 +0000

    Read the local processos from System.Diagnostics.

---
+++ ElectronNet.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ElectronNet.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Process> Processes { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var items = Process
+                            .GetProcesses()
+                            .Where(p => !string.IsNullOrEmpty(p.ProcessName)
+                                        //&& !p.ProcessName.Length
+                                        );
+            Processes = items.ToList();
+        }
+    }
+}
--- ElectronNet.WebApp/Program.cs
+++ ElectronNet.WebApp/Program.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ElectronNET.API;
 
-namespace ElectronNet.API
+namespace ElectronNet.WebApp
 {
     public class Program
     {
@@ -20,6 +15,12 @@ namespace ElectronNet.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    //todo: Electron
+                    webBuilder.UseElectron(args);
+                    //todo: Electron
+                    webBuilder.UseEnvironment("Development");
+
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
